93e5f1b786cdfec5a742217b69e2bbdbd345de40
rbb86 <rbb86@abv.bg>
2021-01-08 23:44:25 +0200

    feat: add all apges

--- config/express.js
+++ config/express.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
 
 module.exports = (app) => {
     
-    app.engine('.hbs', handlebars());
+    app.engine('.hbs', handlebars({
+        extname: '.hbs'
+    }));
     app.set('view engine', '.hbs');
 
-    //TODO: Setup the static files
+    app.use('/static', express.static('static'))
 
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
-// TODO: Require Controllers...
+const express = require("../config/express");
 
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.render('index')
+const {Router} = require('express')
+
+const router = new Router();
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: "Cube workshop"
     })
-};
+})
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About | Cube Workshop'
+    })
+})
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create Cube | Cube Workshop'
+    })
+})
+
+router.get('/details/:id', (req, res) => {
+    res.render('details', {
+        title: 'Details | Cube Workshop'
+    })
+})
+
+router.get('*', (req, res) => {
+    res.render('404', {
+        title: "Error | Cube Works"
+    })
+})
+
+module.exports = router
